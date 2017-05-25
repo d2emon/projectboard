e8198ce9f5ec4b2deb80c3fa6815beadc35f55f1
Dmitry Kutsenko <d2emonium@gmail.com>
2017-05-25 09:38:09 +0300

    Top menu

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from . import views
 
@@ -11,4 +11,8 @@ urlpatterns = [
     # url(r'^auth/', include('django.contrib.auth')),
     # (r'^accounts/', include('registration.backends.default.urls')),
     # (r'^(?P<project_name>\w+)/user/(?P<username>\w+)/$', 'user_details'),
+
+    # Dummy views
+    url(r'^list$', views.login, name='list'),
+    url(r'^settings$', views.login, name='settings'),
 ]
