59add4894f14af402b4d939757801b213ff015f2
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-10 16:00:33 +0300

    global templates

--- projects/views.py
+++ projects/views.py
@@ -56,7 +56,7 @@ def dashboard(request):
         'createform': createform,
         'invites': invites,
     }
-    return render(request, 'project/dashboard.pug', context)
+    return render(request, 'projects/dashboard.pug', context)
 
 
 @login_required
