93b2d299a8ca9e6bd8d0e5ad1f0fff247ee80c36
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-11 03:12:30 +0300

    Signals for user

--- users/models.py
+++ users/models.py
@@ -3,12 +3,23 @@ import random
 
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     avatar = models.ImageField(blank=True, upload_to="avatars/")
-
+    
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+        
     def notifications(self):
         notifications = {
             'updates': 0,
