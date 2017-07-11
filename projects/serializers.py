fe5c742f9b03ecd3ee3f17efff4ba21cf260e21d
Dmitry Kutsenko <d2emonium@gmail.com>
2017-07-11 23:22:20 +0300

    Dummy accept & decline

--- projects/serializers.py
+++ projects/serializers.py
@@ -46,7 +46,16 @@ class UserStatusSerializer(serializers.HyperlinkedModelSerializer):
         )
 
 
-class InviteUserSerializer(serializers.ModelSerializer):
+class InviteUserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='invites-detail',
+    )
+    accept = serializers.HyperlinkedIdentityField(
+        view_name='invites-accept',
+    )
+    decline = serializers.HyperlinkedIdentityField(
+        view_name='invites-decline',
+    )
     project = serializers.SlugRelatedField(
         queryset=Project.objects.all(),
         slug_field='slug',
@@ -59,6 +68,9 @@ class InviteUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectUser
         fields = (
+            'url',
+            'accept',
+            'decline',
             'project',
             'user',
             'status',
